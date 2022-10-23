de11d94a82540aeb103f65c88b47f09b90b9db9b
Jonathan Pulsifer <jonathan@pulsifer.ca>
2022-10-23 15:49:10 -0300

    yeet
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- apps/wishlist/lib/firebase-ssr.ts
+++ apps/wishlist/lib/firebase-ssr.ts
@@ -1,15 +1,31 @@
 import { FirebaseError } from 'firebase-admin';
-import { applicationDefault, getApp, getApps, initializeApp } from 'firebase-admin/app';
+import {
+  applicationDefault,
+  cert,
+  getApp,
+  getApps,
+  initializeApp,
+} from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 
-const databaseURL = process.env.NODE_ENV === 'development'
-? `http://${process.env.FIREBASE_FIRESTORE_EMULATOR_HOST}:${process.env.FIREBASE_FIRESTORE_EMULATOR_PORT}`
-: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`;
+const databaseURL =
+  process.env.NODE_ENV === 'development'
+    ? `http://${process.env.FIREBASE_FIRESTORE_EMULATOR_HOST}:${process.env.FIREBASE_FIRESTORE_EMULATOR_PORT}`
+    : `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`;
+
+const credential =
+  process.env.NODE_ENV === 'development'
+    ? applicationDefault()
+    : cert({
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      });
 
 const firebaseConfig = {
-  credential: applicationDefault(),
-  databaseURL
+  credential,
+  databaseURL,
 };
 
 const admin = getApps().length ? getApp() : initializeApp(firebaseConfig);
