c711b4423dbc43593f0b6e13f776dccccbe08365
Jonathan Pulsifer <jonathan@pulsifer.ca>
2022-10-24 01:26:03 -0300

    yeet
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,24 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["next", "turbo", "plugin:prettier/recommended", 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
   },
+  "env": {
+    "browser": true,
+    "node": true,
+    "jest": true
+  },
+  "plugins": [
+    "react",
+    "@typescript-eslint"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 2020,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  }
 };
