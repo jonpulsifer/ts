c8dc470805950b0508a6c7e5f1e89b24a37d9931
Jonathan Pulsifer <jpulsifer@moonpay.com>
2023-01-02 14:07:59 -0400

    yeet
    Signed-off-by: Jonathan Pulsifer <jpulsifer@moonpay.com>

--- apps/wishlist/lib/firebase-ssr.ts
+++ apps/wishlist/lib/firebase-ssr.ts
@@ -1,39 +1,10 @@
 import { FirebaseError } from 'firebase-admin';
-import {
-  applicationDefault,
-  cert,
-  getApp,
-  getApps,
-  initializeApp,
-} from 'firebase-admin/app';
-import { getAuth } from 'firebase-admin/auth';
-import { FieldPath, getFirestore } from 'firebase-admin/firestore';
+import { FieldPath } from 'firebase-admin/firestore';
 import { cookies } from 'next/headers';
 import { notFound, redirect } from 'next/navigation';
 import { AppUser, Family, Gift } from '../types';
+import { auth, db } from './firebase-ssr-db';
 
-const databaseURL =
-  process.env.NODE_ENV === 'development'
-    ? `http://${process.env.FIREBASE_FIRESTORE_EMULATOR_HOST}:${process.env.FIREBASE_FIRESTORE_EMULATOR_PORT}`
-    : `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`;
-
-const credential =
-  process.env.NODE_ENV === 'development'
-    ? applicationDefault()
-    : cert({
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      });
-
-const firebaseConfig = {
-  credential,
-  databaseURL,
-};
-
-const admin = getApps().length ? getApp() : initializeApp(firebaseConfig);
-const db = getFirestore(admin);
-const auth = getAuth(admin);
 function isFirebaseError(error: unknown): error is FirebaseError {
   return (error as FirebaseError).code !== undefined;
 }
@@ -278,9 +249,6 @@ const getUserGifts = async (id: string) => {
 };
 
 export {
-  admin,
-  auth,
-  db,
   getAllUserGifts,
   getClaimedGifts,
   getFamilies,
--- apps/wishlist/pages/api/stats.ts
+++ apps/wishlist/pages/api/stats.ts
@@ -1,16 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from 'next';
-import { db } from '../../lib/firebase-ssr';
+import { db } from '../../lib/firebase-ssr-db';
 
-export default async function healthz(
+export default async function stats(
   _req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const gifts = await db.collection('gifts').count();
-  const users = await db.collection('users').count();
-  const claimed = await db
-    .collection('gifts')
-    .where('claimed_by', '!=', '')
-    .count();
+  const gifts = db.collection('gifts').count();
+  const users = db.collection('users').count();
+  const claimed = db.collection('gifts').where('claimed_by', '!=', '').count();
   res.status(200).json({ gifts, users, claimed });
 }
