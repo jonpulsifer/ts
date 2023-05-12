ea84a7acc41e3404730fccd0f04a6ca7468cd47c
Jonathan Pulsifer <jonathan@moonpay.com>
2023-05-12 19:45:35 -0300

    Removed code.
    Signed-off-by: Jonathan Pulsifer <jonathan@moonpay.com>

--- apps/headerz/app/api/headers/route.ts
+++ apps/headerz/app/api/headers/route.ts
@@ -2,9 +2,7 @@
 import { NextResponse } from 'next/server';
 import { headers } from 'next/headers';
 
-export async function GET(
-  request: Request,
-) {
+export async function GET(request: Request) {
   const res = Object.create(null);
   for (const [key, value] of headers().entries()) {
     res[key] = value;
