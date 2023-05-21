9124594763335d20a3017a5eacd012dd717ef1ae
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-05-21 20:28:19 -0300

    I am Spartacus
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- apps/wishlist/app/family/join/page.tsx
+++ apps/wishlist/app/family/join/page.tsx
@@ -1,5 +1,4 @@
 import { getFamilies } from '../../../lib/firebase-ssr';
-import { redirect } from 'next/navigation';
 import FamilyList from '../../../components/FamilyList';
 import { Suspense } from 'react';
 import Loading from './loading';
@@ -7,7 +6,6 @@ import Frame from '../../../components/Frame';
 
 const JoinFamilyPage = async () => {
   const { families, user } = await getFamilies();
-  if (!user) redirect('/login');
   return (
     <Frame title="Join a Family">
       <Suspense fallback={<Loading />}>
