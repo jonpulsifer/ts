4dccecacfd28572b10b8271b96eb0fe57c499d31
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-06-13 02:22:43 -0300

    Update UserForm.tsx

--- apps/wishlist/components/UserForm.tsx
+++ apps/wishlist/components/UserForm.tsx
@@ -152,7 +152,7 @@ const UserForm = ({ user }: Props) => {
               autoComplete="shoe-size"
               className="form-control block w-full px-4 py-2 text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none dark:border-gray-800 dark:text-gray-400 dark:focus:text-gray-200 dark:bg-gray-900 dark:focus:bg-gray-800 dark:placeholder-gray-700"
               placeholder="7.5"
-              inputMode="numeric"
+              inputMode="decimal"
               value={shoeSize}
               onChange={(e) => setShoeSize(e.target.value)}
             />
