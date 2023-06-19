5a4189b459727e48ab252e63c0dac2d288693342
Jonathan Pulsifer <jonathan@moonpay.com>
2023-06-19 19:06:32 -0300

    forgot we're not using a smart language
    Signed-off-by: Jonathan Pulsifer <jonathan@moonpay.com>

--- apps/wishlist/app/head.tsx
+++ apps/wishlist/app/head.tsx
@@ -1,10 +1,13 @@
 export default function Head() {
   return (
     <>
-			<title>wishin.app</title>
+      <title>wishin.app</title>
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="icon" href="/favicon.ico" />
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
+      />
     </>
   );
 }
