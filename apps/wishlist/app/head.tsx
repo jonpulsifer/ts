89cf800b29885ccd2c703fbf9076a0e62d294409
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-06-21 18:57:06 -0300

    Update head.tsx
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- apps/wishlist/app/head.tsx
+++ apps/wishlist/app/head.tsx
@@ -6,7 +6,7 @@ export default function Head() {
       <link rel="icon" href="/favicon.ico" />
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1"
       />
     </>
   );
