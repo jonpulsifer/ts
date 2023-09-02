d206c98073ac1845214fbd820925ac124fa8ad55
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-09-02 14:09:49 -0300

    This is supposed to crash
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './src/**/*.{js,ts,jsx,tsx}',
-    '../../packages/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -8,6 +8,6 @@ export default defineConfig((options: Options) => ({
   dts: true,
   minify: true,
   clean: true,
-  external: ['react'],
+  external: ['react', 'next'],
   ...options,
 }));
