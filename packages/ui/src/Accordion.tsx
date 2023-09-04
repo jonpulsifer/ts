771a30796e006ccd6580c92895aad6beff85dea1
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-09-03 22:45:24 -0300

    Update Accordion.tsx
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- packages/ui/src/Accordion.tsx
+++ packages/ui/src/Accordion.tsx
@@ -135,7 +135,7 @@ const Accordion = ({
     >
       <div className="sm:text-left">
         {headerMarkup}
-        <div style={{ height: height }} className="overflow-y-scroll">
+        <div style={{ height: height }} className="overflow-x-scroll">
           {children}
         </div>
       </div>
