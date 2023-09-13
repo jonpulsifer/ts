0428ae58163f5e306275bf9f94f81fce5341c44a
Jonathan Pulsifer <jonathan@moonpay.com>
2023-09-13 18:37:23 -0300

    Pipeline goes brrrrrrr
    Signed-off-by: Jonathan Pulsifer <jonathan@moonpay.com>

--- apps/request-headers/app/api/headers/route.ts
+++ apps/request-headers/app/api/headers/route.ts
@@ -1,10 +1,18 @@
 import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
 
+// import file from filesystem
+import * as myJson from './data/my.json';
+
 export async function GET() {
   const res = Object.create(null);
   for (const [key, value] of headers().entries()) {
     res[key] = value;
   }
+  // do stuff with file to get data
+  const { data } = myJson;
+
+  // use data
+  res['fromFile'] = data;
   return NextResponse.json({ ...res });
 }
