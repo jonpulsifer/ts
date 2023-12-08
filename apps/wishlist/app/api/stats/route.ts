cafb00baad2aa86d8b154389f9033de9e01d4366
Jonathan Pulsifer <jonathan@moonpay.com>
2023-12-08 00:44:23 -0400

    fuckup.
    Signed-off-by: Jonathan Pulsifer <jonathan@moonpay.com>

--- apps/wishlist/app/api/stats/route.ts
+++ apps/wishlist/app/api/stats/route.ts
@@ -2,8 +2,8 @@ import { prisma } from 'lib/prisma';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const gifts = prisma.gift.count();
-  const users = prisma.user.count();
-  const claimed = prisma.gift.count({ where: { claimed: true } });
+  const gifts = await prisma.gift.count();
+  const users = await prisma.user.count();
+  const claimed = await prisma.gift.count({ where: { claimed: true } });
   return NextResponse.json({ gifts, users, claimed });
 }
