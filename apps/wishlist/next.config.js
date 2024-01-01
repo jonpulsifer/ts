eb1b93874aee7a26aef3a420d4abecc184878c42
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-01-01 14:47:35 -0500

    WAHYU SUCKS
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- apps/wishlist/next.config.js
+++ apps/wishlist/next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+    ],
   },
   transpilePackages: ['ui'],
   async headers() {
