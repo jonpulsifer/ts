2244ae222c123f727d1b51f06111d420b0dbcb1b
Jonathan Pulsifer <jonathan@moonpay.com>
2024-01-10 23:17:32 -0400

    Copy pasta fail. still had a instead of a
    Signed-off-by: Jonathan Pulsifer <jonathan@moonpay.com>

--- apps/authme/app/api/auth/[...nextauth]/route.ts
+++ apps/authme/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,33 @@
+import fs from 'fs';
 import { authOptions } from 'lib/auth';
 import NextAuth from 'next-auth';
 
 const handler = NextAuth(authOptions);
+
+console.log('Initializing Prisma on the function');
+const SERVER_CA = process.env.SERVER_CA;
+const CLIENT_IDENTITY_PKCS12 = process.env.CLIENT_IDENTITY_PKCS12;
+if (!SERVER_CA || !CLIENT_IDENTITY_PKCS12) {
+  throw new Error('Missing environment variables');
+}
+[
+  {
+    contents: SERVER_CA,
+    path: '/tmp/server-ca.pem',
+  },
+  {
+    contents: CLIENT_IDENTITY_PKCS12,
+    path: '/tmp/client-identity.p12',
+  },
+].forEach((file) => {
+  // if file exists do not attempt to write it
+  if (fs.existsSync(file.path)) {
+    console.log(`File ${file.path} exists, skipping`);
+    return;
+  }
+  const buf = Buffer.from(file.contents, 'base64');
+  console.info(`Writing ${file.path} (${buf.length} bytes)`);
+  fs.writeFileSync(file.path, buf);
+});
 
 export { handler as GET, handler as POST };
