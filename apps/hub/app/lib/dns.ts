b2d26b8b8a6f0ec3646346771375f31725de5dc8
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-03-05 20:45:30 -0400

    Update dns.ts
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- apps/hub/app/lib/dns.ts
+++ apps/hub/app/lib/dns.ts
@@ -3,47 +3,25 @@ import { headers as requestHeaders } from 'next/headers';
 
 export const ipToName = async () => {
   const ip = getIpFromHeaders();
-  const name = await resolveIpToName(ip);
 
-  console.log(Date.now(), { name, ip });
+  console.log(Date.now(), { ip });
   if (process.env.NODE_ENV === 'development') {
     return 'Development';
   }
-  if (!name) return 'localhost';
-  const lowerCaseName = name.toLowerCase();
-
   switch (true) {
-    case lowerCaseName.includes('screenpi4'):
-    case lowerCaseName.includes('atomic'):
-    case lowerCaseName.includes('ts4'):
-    case lowerCaseName.includes('tinytower'):
-    case lowerCaseName === '10.2.0.28':
-      return 'Laboratory';
-    case lowerCaseName.includes('homepi4'):
-    case lowerCaseName.includes('10.2.0.23'):
-    case lowerCaseName === 'caldigit':
-      return 'Studio';
-    case undefined:
-      return 'Unknown Host: ' + ip;
+    case ip === '10.1.0.5':
+    case ip === '10.1.0.127':
+		case ip === '10.2.0.28':
+		case 
+      return 'Jonathan';
+    case ip === '10.2.0.23':
+      return 'Constance';
     default:
-      return name;
+      return ip;
   }
 };
 
 export const getIpFromHeaders = () => {
   const headers = requestHeaders();
   return headers.get('x-real-ip') || headers.get('x-forwarded-for') || '';
-};
-
-export const resolveIpToName = async (ip: string) => {
-  try {
-    dns.setServers(['10.2.0.1']); // Use Cloudflare DNS for resolution
-    // Handle private IP addresses differently if needed
-    const hostnames = await dns.reverse(ip);
-    console.log(Date.now(), { hostnames });
-    return hostnames[0]; // Assuming the first hostname is the desired one
-  } catch (error) {
-    console.error(Date.now(), 'DNS resolution error:', error);
-    return ip;
-  }
 };
