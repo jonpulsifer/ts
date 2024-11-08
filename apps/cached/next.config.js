7a52f9930121b38f82bd5485b6e5c704c7e8c0af
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-11-08 10:59:17 -0400

    Update next.config.js
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- apps/cached/next.config.js
+++ apps/cached/next.config.js
@@ -2,10 +2,10 @@
 
 const nextConfig = {
   cacheHandler:
-    process.env.NODE_ENV === 'production'
+    process.env.HAS_REDIS
       ? require.resolve('./lib/cache-handler.js')
       : undefined,
-  cacheMaxMemorySize: process.env.NODE_ENV === 'production' ? 0 : undefined, // disable the default (50mb) in-memory cache
+  cacheMaxMemorySize: process.env.HAS_REDIS ? 0 : undefined, // disable the default (50mb) in-memory cache
   output: process.env.STANDALONE ? 'standalone' : undefined,
 };
 
