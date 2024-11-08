e70d141227a8296b3b9e698955ba3d9414b4b0a2
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-11-08 10:15:34 -0400

    chore: move things around
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- apps/cached/next.config.js
+++ apps/cached/next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  cacheHandler: process.env.HAS_REDIS
-    ? require.resolve('./lib/cache-handler.js')
-    : undefined,
-  cacheMaxMemorySize: process.env.HAS_REDIS ? 0 : undefined, // disable the default (50mb) in-memory cache
+  cacheHandler:
+    process.env.NODE_ENV === 'production'
+      ? require.resolve('./lib/cache-handler.js')
+      : undefined,
+  cacheMaxMemorySize: process.env.NODE_ENV === 'production' ? 0 : undefined, // disable the default (50mb) in-memory cache
   output: process.env.STANDALONE ? 'standalone' : undefined,
 };
 
