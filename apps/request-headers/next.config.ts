65bb64574e80a468f1ad771ebf3ba1815a19dd0b
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-11-10 17:49:26 -0400

    Update next.config.ts

--- apps/request-headers/next.config.ts
+++ apps/request-headers/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_SOME_VARIABLE: process.env.NEXT_PUBLIC_SOME_VARIABLE,
   },
-  output: process.env.IS_DOCKER ? "standalone" : undefined,
+  output: process.env.STANDALONE ? "standalone" : undefined,
 };
 
 export default nextConfig;
