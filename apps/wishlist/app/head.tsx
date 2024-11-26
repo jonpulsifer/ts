dc2713c6931e254dde05247415389c33af4db143
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-11-25 20:01:05 -0400

    test: ᕕ(╯°□°)ᕗ stress-tested for rage quits
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- apps/wishlist/app/head.tsx
+++ apps/wishlist/app/head.tsx
@@ -2,8 +2,8 @@ export default function Head() {
   return (
     <>
       <title>wishin.app</title>
-      <link href="/site.webmanifest" rel="manifest" />
-      <link href="/favicon.ico" rel="icon" />
+      <link href="/manifest.json" rel="manifest" />
+      <link href="/favicon.svg" rel="icon" sizes="any" />
       <meta
         content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1"
         name="viewport"
