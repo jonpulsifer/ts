163bd8429a0a6ef76ed55b4bad23ddffc20a4a93
Jonathan Pulsifer <jonathan@pulsifer.ca>
2025-11-21 21:19:54 +0000

    fix: "ctrl+z" but in real life
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- apps/hub/app/root.tsx
+++ apps/hub/app/root.tsx
@@ -62,7 +62,7 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
       error.status === 404
         ? 'The requested page could not be found.'
         : error.statusText || details;
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
+  } else if (error && error instanceof Error) {
     details = error.message;
     stack = error.stack;
   }
